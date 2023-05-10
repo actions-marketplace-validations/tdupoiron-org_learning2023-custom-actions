925112776e52d7482e60becace8ea233f383edff
Thomas Dupoiron <tdupoiron@github.com>
2023-05-10 09:44:04 +0000

    push action

--- repo.js
+++ repo.js
@@ -1,25 +1,27 @@
+const core = require('@actions/core');
 const { Octokit } = require("@octokit/rest");
 
 const octokit = new Octokit({
-  auth: "mypersonalaccesstoken123",
+  auth: process.env.REPO_OPS_TOKEN,
 });
 
+async function createRepository(repoName, isPrivate) {
 
-async function run() {
-    try {
-        const token = core.getInput('token');
-        const octokit = github.getOctokit(token);
-    
-        const repo = await octokit.repos.createInOrg({
-        org: 'octokit',
-        name: 'hello-world',
-        private: true
-        });
-    
-        console.log(repo);
-    } catch (error) {
-        core.setFailed(error.message);
+    octokit.rest.repos.createForAuthenticatedUser({
+        name: repoName,
+        private: isPrivate,
+    }).then(({ data }) => {
+        core.setOutput("repo_full_name", data.full_name);
+        core.setOutput("repo_id", data.id);
     }
+    ).catch((error) => {
+        core.setFailed(error.response.data.message);
     }
+    );
+}
 
-run();
+// Get Input Parameters
+const repoName = core.getInput('repo_name');
+const isPrivate = core.getInput('repo_is_private');
+
+createRepository(repoName, isPrivate);
