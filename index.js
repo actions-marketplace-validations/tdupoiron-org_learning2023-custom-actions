7690978ee7eb09c4f6d29687a2a743b8e86ec229
Thomas Dupoiron <tdupoiron@github.com>
2023-05-10 14:26:00 +0000

    test wf

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const core = require('@actions/core');
 const { Octokit } = require("@octokit/rest");
 
+// Get Input Parameters
+const repoOwner = core.getInput('repo_owner');
+const repoName = core.getInput('repo_name');
+const repoVisibility = core.getInput('repo_visibility');
+const token = core.getInput('token')
+
 const octokit = new Octokit({
-  auth: core.getInput('token'),
+  auth: token,
 });
 
 async function createRepository(repoOwner, repoName, repoVisibility) {
@@ -12,21 +18,14 @@ async function createRepository(repoOwner, repoName, repoVisibility) {
         name: repoName,
         visibility: repoVisibility
     }).then(({ data }) => {
+        core.setOutput("repo_id", data.id);
         core.setOutput("repo_full_name", data.full_name);
         core.setOutput("repo_url", data.html_url);
-        core.setOutput("repo_id", data.id);
     }
     ).catch((error) => {
         core.setFailed(error.response.data.message);
     }
     );
 }
-
-// Get Input Parameters
-const repoOwner = core.getInput('repo_owner');
-const repoName = core.getInput('repo_name');
-const repoVisibility = core.getInput('repo_visibility');
-
-console.log(`Creating repository ${repoOwner}/${repoName} with visibility ${repoVisibility}`);
 
 createRepository(repoOwner, repoName, repoVisibility);
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   target: 'node',
 
   mode: "production",
-  entry: './repo.js',
+  entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'action.js',
